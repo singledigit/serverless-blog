f05563e77eb5e6d50645859a92d528af89628833
Eric Johnson <ericj@singledigit.net>
2017-05-30 16:41:10 -0600

    initial commit

---
+++ bin/update-cicd.js
@@ -0,0 +1,89 @@
+#!/usr/bin/env node
+
+const params = {
+    GitHubOwner: 'bob',
+    Repo: 'bob',
+    Branch: 'master',
+    Token: '12345',
+    Bucket: 'ej-blog-cft',
+    Profile: 'singledigit'
+}
+
+const shell = require('shelljs');
+const fs = require('fs');
+const _ = require('lodash');
+let stacks, services, deployedServices, toBeDeployed, toBeUpdated;
+
+// get stacks
+let stackCommand = shell.exec(`aws cloudformation list-stacks --profile singledigit`, { silent: true })
+
+if (stackCommand.code !== 0) {
+    shell.echo('Could not get current stacks from AWS')
+    shell.exit(1);
+} else {
+    stacks = JSON.parse(stackCommand.stdout).StackSummaries;
+}
+
+// get local service names
+services = fs.readdirSync('./').filter(item => {
+    return item.indexOf('-service') > -1
+});
+
+// get deployed services
+deployedServices = stacks.filter(item => {
+    return _.endsWith(item.StackName, '-cicd') && (item.StackStatus === 'CREATE_COMPLETE' || item.StackStatus === 'UPDATE_COMPLETE')
+})
+
+toBeDeployed = _.difference(services, deployedServices.map(item => { return _.trimEnd(item.StackName, '-cicd') }));
+
+toBeUpdated = _.intersection(services, deployedServices.map(item => { return _.trimEnd(item.StackName, '-cicd') }));
+
+const callStack = (service, action) => {
+    return new Promise((resolve, reject) => {
+        let stackCommand = shell.exec(
+            `aws cloudformation ${action}-stack --stack-name ${service}-cicd \
+            --template-url https://s3.amazonaws.com/${params.Bucket}/api-cicd.yml \
+            --capabilities CAPABILITY_NAMED_IAM \
+            --profile ${params.Profile} \
+            --parameters ParameterKey=GitHubOwner,ParameterValue=${params.GitHubOwner} \
+            ParameterKey=Repo,ParameterValue=${params.Repo} \
+            ParameterKey=Branch,ParameterValue=${params.Branch} \
+            ParameterKey=GitHubOwner,ParameterValue=${params.GitHubOwner} \
+            ParameterKey=Token,ParameterValue=${params.Token} \
+            ParameterKey=Service,ParameterValue=${service}`
+        )
+
+        if (stackCommand.code !== 0) reject(stackCommand.stderr);
+        else resolve(stackCommand.stdout);
+    })
+}
+
+const updateAll = () => {
+    toBeUpdated.forEach(item => {
+        shell.echo(`Updating ${item}-cicd`);
+        callStack(item, 'update')
+            .then(response => {
+                shell.echo(`${item}-cicd is being updated`);
+            })
+            .catch(error => {
+                shell.echo(`Could not update ${item}-cicd`);
+            })
+    })
+}
+
+const installNew = () => {
+    if(toBeDeployed.length === 0 && process.argv[2] !== '--update') shell.echo(`There are no new services to deploy. Add '--update' to update current pipelines`);
+    toBeDeployed.forEach(item => {
+        shell.echo(`Deploying ${item}-cicd`);
+        callStack(item, 'create')
+            .then(response => {
+                shell.echo(`${item}-cicd is being created`);
+            })
+            .catch(error => {
+                shell.echo(`Could not create ${item}-cicd`);
+            })
+    });
+    if (process.argv[2] === '--update') updateAll();
+}
+
+installNew();
