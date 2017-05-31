f8748266fb65a661bac8d808d7164a5dc2370ad4
Eric Johnson <ericj@singledigit.net>
2017-05-30 20:57:20 -0600

    renamed to agent and removed profile... again ... again

--- bin/build.js
+++ bin/build.js
@@ -4,7 +4,7 @@ const shell = require('shelljs');
 // If Agent Service then run Agent and exit
 if(process.env.SERVICE === "agent-service"){
     // need to run CICD Service Agent
-    let agentCommand = shell.exec(`./bin/update-cicd.js`);
+    let agentCommand = shell.exec(`./bin/agent.js`);
 
     if(agentCommand.code !== 0){
         shell.echo('AGENT_ERROR Problem building CICD Services');
