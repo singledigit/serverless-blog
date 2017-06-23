6318ba3d59c9a7ec78eebc4f2fe23ae3e829cc86
Eric Johnson <ericj@singledigit.net>
2017-06-23 14:33:21 -0700

    ERROR_INSTALL in place

--- bin/install.js
+++ bin/install.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 const shell = require('shelljs');
 
-let command = shell.exec('foo');
+let command = shell.exec('foo', {silent: true});
 
 if (command.code !== 0) {
-    shell.echo('INSTALL_ERROR: Error installing stuff');
+    shell.echo(`INSTALL_ERROR: ${command.stderr}`);
     shell.exit(command.code);
 }
