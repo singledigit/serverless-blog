71d480d5ca5cdfc6533a853dad7c174ae75e000a
Eric Johnson <ericj@singledigit.net>
2017-07-26 17:05:29 +0800

    Added read file

---
+++ post-service/read.js
@@ -0,0 +1,10 @@
+//@ts-check
+
+const _ = require('lodash');
+const AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB();
+const table = new AWS.DynamoDB.DocumentClient({ service: dynamodb });
+
+export const handler = (event, context, callback) => {
+    callback(null, {message: "My Lambda Worked"})
+}
